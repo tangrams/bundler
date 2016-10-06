efa4c29dd856076f85547eb27c0329dcc7884aba
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-10-06 15:42:46 -0400

    todos... and cleaning

--- bundler.py
+++ bundler.py
@@ -4,17 +4,20 @@ import os, sys, glob, yaml, zipfile
 
 # Append yaml dependences in yaml_file ('import' files) to another yaml file (full_yaml_file)
 def addDependencies(file_list, filename):
-    # print 'Checking for dependencies on:', os.path.normpath(filename)
     file_list.append(os.path.normpath(filename))
     folder = os.path.dirname(filename)
     yaml_file = yaml.safe_load(open(filename))
 
-    # Search for fonts or textures urls
+    # TODO:
+    #   - Check if any url actually excist and is local before doing nothing
+
+    # Search for fonts urls
     if 'fonts' in yaml_file:
         for font in yaml_file['fonts']:
             if 'url' in yaml_file['fonts'][font]:
                 file_list.append(os.path.normpath(folder + '/' + yaml_file['fonts'][font]['url']))
 
+    # Search for textures urls
     if 'textures' in yaml_file:
         for font in yaml_file['textures']:
             if 'url' in yaml_file['textures'][font]:
@@ -27,18 +30,6 @@ def addDependencies(file_list, filename):
         else:
             for file in yaml_file['import']:
                 addDependencies(file_list, folder + '/' + file)
-
-def zip(files, dst):
-    zf = zipfile.ZipFile("%s.zip" % (dst), "w", zipfile.ZIP_DEFLATED)
-    abs_src = os.path.abspath(src)
-    for dirname, subdirs, files in os.walk(src):
-        for filename in files:
-            absname = os.path.abspath(os.path.join(dirname, filename))
-            arcname = absname[len(abs_src) + 1:]
-            print 'zipping %s as %s' % (os.path.join(dirname, filename),
-                                        arcname)
-            zf.write(absname, arcname)
-    zf.close()
 
 # ================================== Main functions
 def bundler(full_filename):
