f88bafa253d40b2ac57bc070c5feccaa7084d293
Nathaniel Kelso <nvkelso@gmail.com>
2017-01-23 16:38:15 -0800

    look for shader textures, too

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'tangram_bundler',
     url = 'https://github.com/tangrams/bundler',
-    version = 0.2,
+    version = 0.3,
     install_requires = [
         'PyYAML == 3.12'
     ],
--- tangram_bundler/__init__.py
+++ tangram_bundler/__init__.py
@@ -29,6 +29,42 @@ def addDependencies(file_list, filename):
             if 'url' in yaml_file['textures'][font]:
                 file_list.append(os.path.normpath(folder + '/' + yaml_file['textures'][font]['url']))
 
+    # Search for u_envmap urls
+    if 'styles' in yaml_file:
+        for style in yaml_file['styles']:
+            if 'shaders' in yaml_file['styles'][style]:
+                if 'uniforms' in yaml_file['styles'][style]['shaders']:
+                    if 'u_envmap' in yaml_file['styles'][style]['shaders']['uniforms']:
+                        file_list.append(os.path.normpath(folder + '/' + yaml_file['styles'][style]['shaders']['uniforms']['u_envmap']))
+
+# styles:
+#     building-grid:
+#         base: polygons
+#         lighting: false
+#         mix: [hsv, scale-buildings]
+#         texcoords: true
+#         shaders:
+#             uniforms:
+#                 u_tex_grid: building-grid
+#             defines:
+#                 WALL_TINT: vec3(0.950, 0.950, 0.950)
+#             blocks:
+#                 color: |
+#                     if (dot(vec3(0., 0., 1.), worldNormal()) < 1.0 - TANGRAM_EPSILON) {
+#                         // If it's a wall
+#                         color.rgb = hsv2rgb(rgb2hsv(color.rgb) * WALL_TINT);
+#                         color.rgb = mix(color.rgb, vec3(0.),
+#                                         texture2D(u_tex_grid, v_texcoord).a);
+#                     }
+#     terrain:
+#         base: polygons
+#         lighting: false
+#         raster: normal
+#         shaders:
+#             uniforms:
+#                 u_envmap: images/draw-test9.jpg
+
+
     # Search for inner dependemcies
     if 'import' in yaml_file:
         if (type(yaml_file['import']) is str):
