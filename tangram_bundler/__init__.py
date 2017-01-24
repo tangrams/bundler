891ccc18fd83bc51b37e2cbe5fb3c2700352b5ad
Nathaniel Kelso <nvkelso@gmail.com>
2017-01-23 16:39:33 -0800

    remove reference code for u_envmap

--- tangram_bundler/__init__.py
+++ tangram_bundler/__init__.py
@@ -37,34 +37,6 @@ def addDependencies(file_list, filename):
                     if 'u_envmap' in yaml_file['styles'][style]['shaders']['uniforms']:
                         file_list.append(os.path.normpath(folder + '/' + yaml_file['styles'][style]['shaders']['uniforms']['u_envmap']))
 
-# styles:
-#     building-grid:
-#         base: polygons
-#         lighting: false
-#         mix: [hsv, scale-buildings]
-#         texcoords: true
-#         shaders:
-#             uniforms:
-#                 u_tex_grid: building-grid
-#             defines:
-#                 WALL_TINT: vec3(0.950, 0.950, 0.950)
-#             blocks:
-#                 color: |
-#                     if (dot(vec3(0., 0., 1.), worldNormal()) < 1.0 - TANGRAM_EPSILON) {
-#                         // If it's a wall
-#                         color.rgb = hsv2rgb(rgb2hsv(color.rgb) * WALL_TINT);
-#                         color.rgb = mix(color.rgb, vec3(0.),
-#                                         texture2D(u_tex_grid, v_texcoord).a);
-#                     }
-#     terrain:
-#         base: polygons
-#         lighting: false
-#         raster: normal
-#         shaders:
-#             uniforms:
-#                 u_envmap: images/draw-test9.jpg
-
-
     # Search for inner dependemcies
     if 'import' in yaml_file:
         if (type(yaml_file['import']) is str):
